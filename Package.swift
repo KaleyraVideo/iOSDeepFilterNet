12ea377828d1fec913cf956ecfcd15992c39606d
GitHub Action <action@github.com>
2025-06-29 00:27:57 +0000

    Update Package.swift for release 0.0.37

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DeepFilterNet",
-            url: "https://github.com/KaleyraVideo/iOSDeepFilterNet/releases/download/0.0.36/DeepFilterNet.xcframework.zip",
-            checksum: "5f92e941c624731c82caa652f167f2e3bf0cc29dae79c5d34c86a53b0bc7504e"
+            url: "https://github.com/KaleyraVideo/iOSDeepFilterNet/releases/download/0.0.37/DeepFilterNet.xcframework.zip",
+            checksum: "d9d208ec08d56afba9e6b67d1a574f4cccd1214e2d0908017e4ba1acbe653c09"
         ),
     ]
 )
